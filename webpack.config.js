62637e9415559a1703251567858c9521844ab5a8
itreverie <brendadhk@gmail.com>
2017-11-03 13:03:28 -0400

    separating vendors, minigying bundles, diff node config, diff babel config, multiple node instances.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require ('webpack');
 
 //adding the resolve section allow us to tell webpack that if it sees a require, it also need to look in the
 //lib and node_modules folder
@@ -7,16 +8,38 @@ const config = {
         path.resolve('./lib'),
         path.resolve('./node_modules'),
     ]},
-    entry: [ 'babel-polyfill', './lib/renderers/dom.js'],
+    entry: {
+        vendor: ['babel-polyfill', 'react', 'react-dom', 'prop-types', 'axios', 'lodash.debounce', 'lodash.pickby'],
+        app: ['./lib/renderers/dom.js']
+    },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude:/node_modules/, use: 'babel-loader' }
+            {
+                test: /\.js$/, exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets:['react', 'env', 'stage-2']
+                    }
+                }
+            }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            // filename: "vendor.js"
+            // (Give the chunk a different name)
+
+            minChunks: Infinity,
+            // (with more entries, this ensures that no other module
+            //  goes into the vendor chunk)
+        })
+    ]
 };
 
 module.exports = config;
